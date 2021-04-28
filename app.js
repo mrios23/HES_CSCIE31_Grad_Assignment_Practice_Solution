77822083d4516a55091e1c9e1e840a5741453e65
Mya Rios <myarios23@gmail.com>
2021-04-27 22:16:52 -0400

    Adding the solution to the practice problem

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var buttonRouter = require('./routes/buttonRouter');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/clickedButton', buttonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/buttonRouter.js
+++ routes/buttonRouter.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET clickedButton page. */
+router.get('/', function(req, res, next) {
+    res.render('clickedButton');
+  });
+
 module.exports = router;
